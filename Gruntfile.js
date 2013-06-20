3ff3fb7b36e1a43b78a8ebfa4884e0cb0f8353c4
claudiosmweb <contato@claudiosmweb.com>
2013-06-20 18:32:28 -0300

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,133 @@
+module.exports = function(grunt) {
+"use strict";
+
+    grunt.initConfig({
+
+        // gets the package vars
+        pkg: grunt.file.readJSON("package.json"),
+        svn_settings: {
+            path: "../../../../wp_plugins/<%= pkg.name %>",
+            tag: "<%= svn_settings.path %>/tags/<%= pkg.version %>",
+            trunk: "<%= svn_settings.path %>/trunk",
+            exclude: [
+                ".git/",
+                ".gitignore",
+                ".sass-cache/",
+                "node_modules/",
+                "assets/sass/",
+                "Gruntfile.js",
+                "README.md",
+                "package.json",
+                "config.rb",
+                "*.zip"
+            ]
+        },
+
+        // javascript linting with jshint
+        jshint: {
+            options: {
+                "bitwise": true,
+                "eqeqeq": true,
+                "eqnull": true,
+                "immed": true,
+                "newcap": true,
+                "es5": true,
+                "esnext": true,
+                "latedef": true,
+                "noarg": true,
+                "node": true,
+                "undef": false,
+                "browser": true,
+                "trailing": true,
+                "jquery": true,
+                "curly": true
+            },
+            all: [
+                "Gruntfile.js"
+            ]
+        },
+
+        // compass and scss
+        compass: {
+            dist: {
+                options: {
+                    config: "config.rb",
+                    force: true,
+                    outputStyle: "compressed"
+                }
+            }
+        },
+
+        // watch for changes and trigger compass
+        watch: {
+            compass: {
+                files: [
+                    "assets/sass/**"
+                ],
+                tasks: ["compass"]
+            }
+        },
+
+        // rsync commands used to take the files to svn repository
+        rsync: {
+            tag: {
+                src: "./",
+                dest: "<%= svn_settings.tag %>",
+                recursive: true,
+                exclude: "<%= svn_settings.exclude %>"
+            },
+            trunk: {
+                src: "./",
+                dest: "<%= svn_settings.trunk %>",
+                recursive: true,
+                exclude: "<%= svn_settings.exclude %>"
+            }
+        },
+
+        // shell command to commit the new version of the plugin
+        shell: {
+            svn_add: {
+                command: 'svn add --force * --auto-props --parents --depth infinity -q',
+                options: {
+                    stdout: true,
+                    stderr: true,
+                    execOptions: {
+                        cwd: "<%= svn_settings.path %>"
+                    }
+                }
+            },
+            svn_commit: {
+                command: "svn commit -m 'updated the plugin version to <%= pkg.version %>'",
+                options: {
+                    stdout: true,
+                    stderr: true,
+                    execOptions: {
+                        cwd: "<%= svn_settings.path %>"
+                    }
+                }
+            }
+        }
+    });
+
+    // load tasks
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-compass");
+    grunt.loadNpmTasks("grunt-rsync");
+    grunt.loadNpmTasks("grunt-shell");
+
+    // default task
+    grunt.registerTask("default", [
+        "jshint",
+        "compass"
+    ]);
+
+    // deploy task
+    grunt.registerTask("deploy", [
+        "default",
+        "rsync:tag",
+        "rsync:trunk",
+        "shell:svn_add",
+        "shell:svn_commit"
+    ]);
+};
--- config.rb
+++ config.rb
@@ -1,15 +1,15 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "js"
+http_path = ""
+css_dir = "assets/css"
+sass_dir = "assets/sass"
+images_dir = "assets/images"
+javascripts_dir = "assets/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
+output_style = (environment == :production) ? :compressed : :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
