1ae471c71a0e26e0a5fba26278978e4e4743d819
claudiosmweb <contato@claudiosmweb.com>
2013-09-03 18:55:47 -0300

    updated the counters

--- includes/class-counter.php
+++ includes/class-counter.php
@@ -57,144 +57,130 @@ class Social_Count_Plus_Counter {
             && ! empty( $settings['twitter_consumer_secret'] )
             && ! empty( $settings['twitter_access_token'] )
             && ! empty( $settings['twitter_access_token_secret'] )
+            && ! empty( $settings['twitter_user'] )
         ) {
-            // Test if twitter_user is empty.
-            if ( ! empty( $settings['twitter_user'] ) ) {
-                $twitter_user = $settings['twitter_user'];
+            $twitter_user = $settings['twitter_user'];
 
-                $twitter_data_params = array(
-                    'method'    => 'GET',
-                    'sslverify' => false,
-                    'timeout'   => 30,
-                    'headers'   => array(
-                        'Content-Type'  => 'application/x-www-form-urlencoded',
-                        'Authorization' => $this->twitter_oauth(
-                            $twitter_user,
-                            $settings['twitter_consumer_key'],
-                            $settings['twitter_consumer_secret'],
-                            $settings['twitter_access_token'],
-                            $settings['twitter_access_token_secret']
-                        )
+            $twitter_data_params = array(
+                'method'    => 'GET',
+                'sslverify' => false,
+                'timeout'   => 30,
+                'headers'   => array(
+                    'Content-Type'  => 'application/x-www-form-urlencoded',
+                    'Authorization' => $this->twitter_oauth(
+                        $twitter_user,
+                        $settings['twitter_consumer_key'],
+                        $settings['twitter_consumer_secret'],
+                        $settings['twitter_access_token'],
+                        $settings['twitter_access_token_secret']
                     )
-                );
+                )
+            );
 
-                // Get twitter data.
-                $twitter_data = wp_remote_get( 'https://api.twitter.com/1.1/users/show.json?screen_name=' . $twitter_user, $twitter_data_params );
+            // Get twitter data.
+            $twitter_data = wp_remote_get( 'https://api.twitter.com/1.1/users/show.json?screen_name=' . $twitter_user, $twitter_data_params );
 
-                if ( is_wp_error( $twitter_data ) ) {
-                    $count['twitter'] = ( isset( $cache['twitter'] ) ) ? $cache['twitter'] : 0;
+            if ( is_wp_error( $twitter_data ) ) {
+                $count['twitter'] = ( isset( $cache['twitter'] ) ) ? $cache['twitter'] : 0;
+            } else {
+                $twitter_response = json_decode( $twitter_data['body'], true );
+
+                if ( isset( $twitter_response['followers_count'] ) ) {
+                    $twitter_count = $twitter_response['followers_count'];
+
+                    $count['twitter'] = $twitter_count;
+                    $cache['twitter'] = $twitter_count;
                 } else {
-                    $twitter_response = json_decode( $twitter_data['body'], true );
-
-                    if ( isset( $twitter_response['followers_count'] ) ) {
-                        $twitter_count = $twitter_response['followers_count'];
-
-                        $count['twitter'] = $twitter_count;
-                        $cache['twitter'] = $twitter_count;
-                    } else {
-                        $count['twitter'] = ( isset( $cache['twitter'] ) ) ? $cache['twitter'] : 0;
-                    }
+                    $count['twitter'] = ( isset( $cache['twitter'] ) ) ? $cache['twitter'] : 0;
                 }
             }
         }
 
         // Facebook.
-        if ( isset( $settings['facebook_active'] ) ) {
-            // Test if facebook_id is empty.
-            if ( ! empty( $settings['facebook_id'] ) ) {
-                $facebook_id = $settings['facebook_id'];
+        if ( isset( $settings['facebook_active'] ) && ! empty( $settings['facebook_id'] ) ) {
 
-                // Get facebook data.
-                $facebook_data = wp_remote_get( 'http://api.facebook.com/restserver.php?method=facebook.fql.query&query=SELECT%20fan_count%20FROM%20page%20WHERE%20page_id=' . $facebook_id );
+            // Get facebook data.
+            $facebook_data = wp_remote_get( 'http://api.facebook.com/restserver.php?method=facebook.fql.query&query=SELECT%20fan_count%20FROM%20page%20WHERE%20page_id=' . $settings['facebook_id'] );
 
-                if ( is_wp_error( $facebook_data ) ) {
-                    $count['facebook'] = ( isset( $cache['facebook'] ) ) ? $cache['facebook'] : 0;
+            if ( is_wp_error( $facebook_data ) ) {
+                $count['facebook'] = ( isset( $cache['facebook'] ) ) ? $cache['facebook'] : 0;
+            } else {
+                $facebook_xml = new SimpleXmlElement( $facebook_data['body'], LIBXML_NOCDATA );
+                $facebook_count = (string) $facebook_xml->page->fan_count;
+
+                if ( $facebook_count ) {
+                    $count['facebook'] = $facebook_count;
+                    $cache['facebook'] = $facebook_count;
                 } else {
-                    $facebook_xml = new SimpleXmlElement( $facebook_data['body'], LIBXML_NOCDATA );
-                    $facebook_count = (string) $facebook_xml->page->fan_count;
-
-                    if ( $facebook_count ) {
-                        $count['facebook'] = $facebook_count;
-                        $cache['facebook'] = $facebook_count;
-                    } else {
-                        $count['facebook'] = ( isset( $cache['facebook'] ) ) ? $cache['facebook'] : 0;
-                    }
+                    $count['facebook'] = ( isset( $cache['facebook'] ) ) ? $cache['facebook'] : 0;
                 }
             }
         }
 
         // YouTube.
-        if ( isset( $settings['youtube_active'] ) ) {
-            // Test if youtube_user is empty.
-            if ( ! empty( $settings['youtube_user'] ) ) {
-                $youtube_user = $settings['youtube_user'];
+        if ( isset( $settings['youtube_active'] ) && ! empty( $settings['youtube_user'] ) ) {
 
-                // Get youtube data.
-                $youtube_data = wp_remote_get( 'http://gdata.youtube.com/feeds/api/users/' . $youtube_user );
+            // Get youtube data.
+            $youtube_data = wp_remote_get( 'http://gdata.youtube.com/feeds/api/users/' . $settings['youtube_user'] );
 
-                if ( is_wp_error( $youtube_data ) || '400' <= $youtube_data['response']['code'] ) {
-                    $count['youtube'] = ( isset( $cache['youtube'] ) ) ? $cache['youtube'] : 0;
+            if ( is_wp_error( $youtube_data ) || '400' <= $youtube_data['response']['code'] ) {
+                $count['youtube'] = ( isset( $cache['youtube'] ) ) ? $cache['youtube'] : 0;
+            } else {
+                $youtube_body = str_replace( 'yt:', '', $youtube_data['body'] );
+                $youtube_xml = new SimpleXmlElement( $youtube_body, LIBXML_NOCDATA );
+                $youtube_count = (string) $youtube_xml->statistics['subscriberCount'];
+
+                if ( $youtube_count ) {
+                    $count['youtube'] = $youtube_count;
+                    $cache['youtube'] = $youtube_count;
                 } else {
-                    $youtube_body = str_replace( 'yt:', '', $youtube_data['body'] );
-                    $youtube_xml = new SimpleXmlElement( $youtube_body, LIBXML_NOCDATA );
-                    $youtube_count = (string) $youtube_xml->statistics['subscriberCount'];
-
-                    if ( $youtube_count ) {
-                        $count['youtube'] = $youtube_count;
-                        $cache['youtube'] = $youtube_count;
-                    } else {
-                        $count['youtube'] = ( isset( $cache['youtube'] ) ) ? $cache['youtube'] : 0;
-                    }
+                    $count['youtube'] = ( isset( $cache['youtube'] ) ) ? $cache['youtube'] : 0;
                 }
             }
         }
 
         // Google Plus.
-        if ( isset( $settings['googleplus_active'] ) ) {
-            // Test if googleplus_id is empty.
-            if ( ! empty( $settings['googleplus_id'] ) ) {
-                $googleplus_id = 'https://plus.google.com/' . $settings['googleplus_id'];
+        if ( isset( $settings['googleplus_active'] ) && ! empty( $settings['googleplus_id'] ) ) {
+            $googleplus_id = 'https://plus.google.com/' . $settings['googleplus_id'];
 
-                $googleplus_data_params = array(
-                    'method'    => 'POST',
-                    'sslverify' => false,
-                    'timeout'   => 30,
-                    'headers'   => array( 'Content-Type' => 'application/json' ),
-                    'body'      => '[{"method":"pos.plusones.get","id":"p","params":{"nolog":true,"id":"' . $googleplus_id . '","source":"widget","userId":"@viewer","groupId":"@self"},"jsonrpc":"2.0","key":"p","apiVersion":"v1"}]'
-                );
+            $googleplus_data_params = array(
+                'method'    => 'POST',
+                'sslverify' => false,
+                'timeout'   => 30,
+                'headers'   => array( 'Content-Type' => 'application/json' ),
+                'body'      => '[{"method":"pos.plusones.get","id":"p","params":{"nolog":true,"id":"' . $googleplus_id . '","source":"widget","userId":"@viewer","groupId":"@self"},"jsonrpc":"2.0","key":"p","apiVersion":"v1"}]'
+            );
 
-                // Get googleplus data.
-                $googleplus_data = wp_remote_get( 'https://clients6.google.com/rpc', $googleplus_data_params );
+            // Get googleplus data.
+            $googleplus_data = wp_remote_get( 'https://clients6.google.com/rpc', $googleplus_data_params );
 
-                if ( is_wp_error( $googleplus_data ) || '400' <= $googleplus_data['response']['code'] ) {
-                    $count['googleplus'] = ( isset( $cache['googleplus'] ) ) ? $cache['googleplus'] : 0;
+            if ( is_wp_error( $googleplus_data ) || '400' <= $googleplus_data['response']['code'] ) {
+                $count['googleplus'] = ( isset( $cache['googleplus'] ) ) ? $cache['googleplus'] : 0;
+            } else {
+                $googleplus_response = json_decode( $googleplus_data['body'], true );
+
+                if ( isset( $googleplus_response[0]['result']['metadata']['globalCounts']['count'] ) ) {
+                    $googleplus_count = $googleplus_response[0]['result']['metadata']['globalCounts']['count'];
+
+                    $count['googleplus'] = $googleplus_count;
+                    $cache['googleplus'] = $googleplus_count;
                 } else {
-                    $googleplus_response = json_decode( $googleplus_data['body'], true );
-
-                    if ( isset( $googleplus_response[0]['result']['metadata']['globalCounts']['count'] ) ) {
-                        $googleplus_count = $googleplus_response[0]['result']['metadata']['globalCounts']['count'];
-
-                        $count['googleplus'] = $googleplus_count;
-                        $cache['googleplus'] = $googleplus_count;
-                    } else {
-                        $count['googleplus'] = ( isset( $cache['googleplus'] ) ) ? $cache['googleplus'] : 0;
-                    }
+                    $count['googleplus'] = ( isset( $cache['googleplus'] ) ) ? $cache['googleplus'] : 0;
                 }
             }
         }
 
-        // Steam
-        if (
-            isset( $settings['steam_active'] )
-            && ! empty( $settings['steam_group_name'] ) ) {
-            $steam_group_page_http_rq = wp_remote_get( 'http://steamcommunity.com/groups/' . $settings['steam_group_name'] . '/memberslistxml/?xml=1' );
-            if ( is_wp_error( $steam_group_page_http_rq ) ) {
+        // Steam.
+        if ( isset( $settings['steam_active'] ) && ! empty( $settings['steam_group_name'] ) ) {
+
+            // Get steam data.
+            $steam_data = wp_remote_get( 'http://steamcommunity.com/groups/' . $settings['steam_group_name'] . '/memberslistxml/?xml=1' );
+
+            if ( is_wp_error( $steam_data ) ) {
                 $count['steam'] = ( isset( $cache['steam'] ) ) ? $cache['steam'] : 0;
             } else {
-                libxml_use_internal_errors(true);
-                $steam_xml = simplexml_load_string( $steam_group_page_http_rq['body'] );
+                $steam_xml = new SimpleXmlElement( $steam_data['body'], LIBXML_NOCDATA );
                 $steam_count = (string) $steam_xml->groupDetails->memberCount;
-                libxml_use_internal_errors(false);
 
                 if ( $steam_count ) {
                     $count['steam'] = $steam_count;
