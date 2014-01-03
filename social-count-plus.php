64634c2a7bad06f6952b5ed22dce6e2033e73c66
claudiosmweb <contato@claudiosmweb.com>
2014-01-03 00:47:47 -0200

    fixed rel nofollow option and added target blank option

--- social-count-plus.php
+++ social-count-plus.php
@@ -334,7 +334,15 @@ class Social_Count_Plus {
 				'type' => 'section',
 				'menu' => 'socialcountplus_settings'
 			),
-			'nofollow' => array(
+			'target_blank' => array(
+				'title' => __( 'Open URLs in new tab/window', 'social-count-plus' ),
+				'default' => 1,
+				'type' => 'checkbox',
+				'section' => 'settings',
+				'menu' => 'socialcountplus_settings',
+				'description' => __( 'This option add target="_blank" in all counters URLs.' )
+			),
+			'rel_nofollow' => array(
 				'title' => __( 'Add nofollow in URLs', 'social-count-plus' ),
 				'default' => 1,
 				'type' => 'checkbox',
@@ -420,6 +428,8 @@ class Social_Count_Plus {
 				'soundcloud_client_id'   => '',
 				'posts_active'           => ( 'true' == get_option( 'scp_show_posts' ) ) ? 1 : '',
 				'comments_active'        => ( 'true' == get_option( 'scp_show_comment' ) ) ? 1 : '',
+				'target_blank'           => 1,
+				// 'rel_nofollow'           => ''
 			);
 
 			$model = 0;
@@ -903,16 +913,20 @@ class Social_Count_Plus {
 	/**
 	 * Construct view li element.
 	 *
-	 * @param  string $slug   Item slug.
-	 * @param  string $url    Item url.
-	 * @param  int    $count  Item count.
-	 * @param  string $title  Item title.
+	 * @param  string $slug     Item slug.
+	 * @param  string $url      Item url.
+	 * @param  int    $count    Item count.
+	 * @param  string $title    Item title.
+	 * @param  array  $settings Item settings.
 	 *
-	 * @return string         HTML li element.
+	 * @return string           HTML li element.
 	 */
-	public function get_view_li( $slug, $url, $count, $title, $color ) {
+	protected function get_view_li( $slug, $url, $count, $title, $color, $settings ) {
+		$target_blank = isset( $settings['target_blank'] ) ? ' target="_blank"' : '';
+		$rel_nofollow = isset( $settings['rel_nofollow'] ) ? ' rel="nofollow"' : '';
+
 		$html = sprintf( '<li class="count-%s">', $slug );
-			$html .= sprintf( '<a class="icon" href="%s" target="_blank" rel="nofollow"></a>', esc_url( $url ) );
+			$html .= sprintf( '<a class="icon" href="%s"%s%s></a>', esc_url( $url ), $target_blank, $rel_nofollow );
 			$html .= '<span class="items">';
 				$html .= sprintf( '<span class="count" style="color: %s !important;">%s</span>', $color, apply_filters( 'social_count_plus_number_format', $count ) );
 				$html .= sprintf( '<span class="label" style="color: %s !important;">%s</span>', $color, $title );
@@ -961,31 +975,31 @@ class Social_Count_Plus {
 			$html .= '<ul class="' . $style . '">';
 
 				// Twitter counter.
-				$html .= ( isset( $settings['twitter_active'] ) ) ? $this->get_view_li( 'twitter', 'http://twitter.com/' . $settings['twitter_user'], $count['twitter'], __( 'followers', 'social-count-plus' ), $color ) : '';
+				$html .= ( isset( $settings['twitter_active'] ) ) ? $this->get_view_li( 'twitter', 'http://twitter.com/' . $settings['twitter_user'], $count['twitter'], __( 'followers', 'social-count-plus' ), $color, $settings ) : '';
 
 				// Facebook counter.
-				$html .= ( isset( $settings['facebook_active'] ) ) ? $this->get_view_li( 'facebook', 'http://www.facebook.com/' . $settings['facebook_id'], $count['facebook'], __( 'likes', 'social-count-plus' ), $color ) : '';
+				$html .= ( isset( $settings['facebook_active'] ) ) ? $this->get_view_li( 'facebook', 'http://www.facebook.com/' . $settings['facebook_id'], $count['facebook'], __( 'likes', 'social-count-plus' ), $color, $settings ) : '';
 
 				// YouTube counter.
-				$html .= ( isset( $settings['youtube_active'] ) ) ? $this->get_view_li( 'youtube', 'http://www.youtube.com/user/' . $settings['youtube_user'], $count['youtube'], __( 'subscribers', 'social-count-plus' ), $color ) : '';
+				$html .= ( isset( $settings['youtube_active'] ) ) ? $this->get_view_li( 'youtube', 'http://www.youtube.com/user/' . $settings['youtube_user'], $count['youtube'], __( 'subscribers', 'social-count-plus' ), $color, $settings ) : '';
 
 				// Google Plus counter.
-				$html .= ( isset( $settings['googleplus_active'] ) ) ? $this->get_view_li( 'googleplus', 'https://plus.google.com/' . $settings['googleplus_id'], $count['googleplus'], __( 'followers', 'social-count-plus' ), $color ) : '';
+				$html .= ( isset( $settings['googleplus_active'] ) ) ? $this->get_view_li( 'googleplus', 'https://plus.google.com/' . $settings['googleplus_id'], $count['googleplus'], __( 'followers', 'social-count-plus' ), $color, $settings ) : '';
 
 				// Instagram counter.
-				$html .= ( isset( $settings['instagram_active'] ) ) ? $this->get_view_li( 'instagram', 'http://instagram.com/' . $settings['instagram_username'], $count['instagram'], __( 'followers', 'social-count-plus' ), $color ) : '';
+				$html .= ( isset( $settings['instagram_active'] ) ) ? $this->get_view_li( 'instagram', 'http://instagram.com/' . $settings['instagram_username'], $count['instagram'], __( 'followers', 'social-count-plus' ), $color, $settings ) : '';
 
 				// Steam counter
-				$html .= ( isset( $settings['steam_active'] ) ) ? $this->get_view_li( 'steam', 'http://steamcommunity.com/groups/' . $settings['steam_group_name'], $count['steam'], __( 'members', 'social-count-plus' ), $color ) : '';
+				$html .= ( isset( $settings['steam_active'] ) ) ? $this->get_view_li( 'steam', 'http://steamcommunity.com/groups/' . $settings['steam_group_name'], $count['steam'], __( 'members', 'social-count-plus' ), $color, $settings ) : '';
 
 				// SoundCloud counter.
-				$html .= ( isset( $settings['soundcloud_active'] ) ) ? $this->get_view_li( 'soundcloud', 'https://soundcloud.com/' . $settings['soundcloud_username'], $count['soundcloud'], __( 'followers', 'social-count-plus' ), $color ) : '';
+				$html .= ( isset( $settings['soundcloud_active'] ) ) ? $this->get_view_li( 'soundcloud', 'https://soundcloud.com/' . $settings['soundcloud_username'], $count['soundcloud'], __( 'followers', 'social-count-plus' ), $color, $settings ) : '';
 
 				// Posts counter.
-				$html .= ( isset( $settings['posts_active'] ) ) ? $this->get_view_li( 'posts', get_home_url(), $count['posts'], __( 'posts', 'social-count-plus' ), $color ) : '';
+				$html .= ( isset( $settings['posts_active'] ) ) ? $this->get_view_li( 'posts', get_home_url(), $count['posts'], __( 'posts', 'social-count-plus' ), $color, $settings ) : '';
 
 				// Comments counter.
-				$html .= ( isset( $settings['comments_active'] ) ) ? $this->get_view_li( 'comments', get_home_url(), $count['comments'], __( 'comments', 'social-count-plus' ), $color ) : '';
+				$html .= ( isset( $settings['comments_active'] ) ) ? $this->get_view_li( 'comments', get_home_url(), $count['comments'], __( 'comments', 'social-count-plus' ), $color, $settings ) : '';
 
 			$html .= '</ul>';
 		$html .= '</div>';
