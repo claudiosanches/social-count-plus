409a307380cbaf6ac79638d3064b8a454e66effa
claudiosmweb <contato@claudiosmweb.com>
2014-05-19 23:25:07 -0300

    improved the plugin admin screen

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,4 +1,27 @@
 jQuery(document).ready(function($) {
 	// Init the color picker.
 	$( '.social-count-plus-color-field' ).wpColorPicker();
+
+	function socialCountPlusShowHide( input ) {
+		var current  = input.attr( 'id' ),
+			id       = current.replace( '_active', '' ),
+			elements = $( '.form-table [id^="' + id + '"]' )
+						.not( '[id^="' + current + '"]' )
+						.parent( 'td' )
+						.parent( 'tr' );
+
+		if ( input.is( ':checked' ) ) {
+			elements.show();
+		} else {
+			elements.hide();
+		}
+	}
+
+	$( '.form-table input[id$="_active"]' ).each( function () {
+		socialCountPlusShowHide( $( this ) );
+	});
+
+	$( '.form-table input[id$="_active"]' ).on( 'click', function () {
+		socialCountPlusShowHide( $( this ) );
+	});
 });
