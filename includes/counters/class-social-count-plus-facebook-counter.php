607ab6a51a46e33cb73eb89022934414a2931044
Claudio Sanches <contato@claudiosmweb.com>
2015-08-20 18:34:45 -0300

    Use the new Facebook Graph API

--- includes/counters/class-social-count-plus-facebook-counter.php
+++ includes/counters/class-social-count-plus-facebook-counter.php
@@ -35,7 +35,30 @@ class Social_Count_Plus_Facebook_Counter extends Social_Count_Plus_Counter {
 	 * @return bool
 	 */
 	public function is_available( $settings ) {
-		return ( isset( $settings['facebook_active'] ) && isset( $settings['facebook_id'] ) && ! empty( $settings['facebook_id'] ) );
+		return isset( $settings['facebook_active'] ) && ! empty( $settings['facebook_id'] ) && ! empty( $settings['facebook_app_id'] ) && ! empty( $settings['facebook_app_secret'] );
+	}
+
+	/**
+	 * Get access token.
+	 *
+	 * @param  array $settings Plugin settings.
+	 *
+	 * @return string
+	 */
+	protected function get_access_token( $settings ) {
+		$url = sprintf(
+			'%s/oauth/access_token?client_id=%s&client_secret=%s&grant_type=client_credentials',
+			$this->api_url,
+			sanitize_text_field( $settings['facebook_app_id'] ),
+			sanitize_text_field( $settings['facebook_app_secret'] )
+		);
+		$access_token = wp_remote_get( $url, array( 'timeout' => 60 ) );
+
+		if ( is_wp_error( $access_token ) || ( isset( $access_token['response']['code'] ) && 200 != $access_token['response']['code'] ) ) {
+			return '';
+		} else {
+			return sanitize_text_field( $access_token['body'] );
+		}
 	}
 
 	/**
@@ -48,14 +71,17 @@ class Social_Count_Plus_Facebook_Counter extends Social_Count_Plus_Counter {
 	 */
 	public function get_total( $settings, $cache ) {
 		if ( $this->is_available( $settings ) ) {
-			$params = array(
-				'sslverify' => false,
-				'timeout'   => 60
+			$access_token = $this->get_access_token( $settings );
+			$url = sprintf(
+				'%s%s?fields=likes&%s',
+				$this->api_url,
+				sanitize_text_field( $settings['facebook_id'] ),
+				$access_token
 			);
 
-			$this->connection = wp_remote_get( $this->api_url . $settings['facebook_id'], $params );
+			$this->connection = wp_remote_get( $url, array( 'timeout' => 60 ) );
 
-			if ( is_wp_error( $this->connection ) ) {
+			if ( is_wp_error( $this->connection ) || ( isset( $this->connection['response']['code'] ) && 200 != $this->connection['response']['code'] ) ) {
 				$this->total = ( isset( $cache[ $this->id ] ) ) ? $cache[ $this->id ] : 0;
 			} else {
 				$_data = json_decode( $this->connection['body'], true );
