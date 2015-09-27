a2fcbaeb20074d49a09132197aeeda6e1a136771
Claudio Sanches <contato@claudiosmweb.com>
2015-09-28 01:04:27 +0200

    Removed old twitter error messsages

--- includes/admin/views/html-settings-page.php
+++ includes/admin/views/html-settings-page.php
@@ -9,27 +9,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<a href="options-general.php?page=social-count-plus&amp;tab=settings" class="nav-tab <?php echo $current_tab == 'settings' ? 'nav-tab-active' : ''; ?>"><?php _e( 'Settings', 'social-count-plus' ); ?></a><a href="options-general.php?page=social-count-plus&amp;tab=design" class="nav-tab <?php echo $current_tab == 'design' ? 'nav-tab-active' : ''; ?>"><?php _e( 'Design', 'social-count-plus' ); ?></a><a href="options-general.php?page=social-count-plus&amp;tab=shortcodes" class="nav-tab <?php echo $current_tab == 'shortcodes' ? 'nav-tab-active' : ''; ?>"><?php _e( 'Shortcodes and Functions', 'social-count-plus' ); ?></a><a href="options-general.php?page=social-count-plus&amp;tab=system_status" class="nav-tab <?php echo $current_tab == 'system_status' ? 'nav-tab-active' : ''; ?>"><?php _e( 'System Status', 'social-count-plus' ); ?></a>
 	</h2>
 
-	<?php
-		if ( isset( $this->plugin_settings['twitter_active'] ) && (
-			empty( $this->plugin_settings['twitter_user'] )
-			|| empty( $this->plugin_settings['twitter_consumer_key'] )
-			|| empty( $this->plugin_settings['twitter_consumer_secret'] )
-			|| empty( $this->plugin_settings['twitter_access_token'] )
-			|| empty( $this->plugin_settings['twitter_access_token_secret'] )
-		) ) :
-	 ?>
-		<div class="error">
-			<p><?php _e( 'To use the counter of Twitter you need to fill the fields:', 'social-count-plus' ); ?></p>
-			<ul style="list-style: disc; margin-left: 20px;">
-				<li><strong><?php _e( 'Twitter username', 'social-count-plus' ); ?></strong></li>
-				<li><strong><?php _e( 'Twitter Consumer key', 'social-count-plus' ); ?></strong></li>
-				<li><strong><?php _e( 'Twitter Consumer secret', 'social-count-plus' ); ?></strong></li>
-				<li><strong><?php _e( 'Twitter Access token', 'social-count-plus' ); ?></strong></li>
-				<li><strong><?php _e( 'Twitter Access token secret', 'social-count-plus' ); ?></strong></li>
-			</ul>
-		</div>
-	<?php endif; ?>
-
 	<?php include 'html-help-us.php'; ?>
 
 	<form method="post" action="options.php">
