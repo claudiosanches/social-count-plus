2eb3dd70a68d1339472004cabf86836491680793
Claudio Sanches <contato@claudiosmweb.com>
2015-12-22 16:16:29 -0200

    Updated help us message

--- includes/admin/views/html-help-us.php
+++ includes/admin/views/html-help-us.php
@@ -3,10 +3,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-$reviews_url = 'https://wordpress.org/support/view/plugin-reviews/social-count-plus?filter=5#postform';
-
 ?>
 
 <div class="updated">
-	<p><?php printf( __( 'Help us keep the %s free making a %s or rate %s on %s. Thank you in advance!', 'social-count-plus' ), '<strong>' . __( 'Social Count Plus', 'social-count-plus' ) . '</strong>', '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=Y8HP99ZJ5Z59L">' . __( 'donation', 'social-count-plus' ) . '</a>', '<a href="' . esc_url( $reviews_url ) . '" target="_blank">&#9733;&#9733;&#9733;&#9733;&#9733;</a>', '<a href="' . esc_url( $reviews_url ) . '" target="_blank">' . __( 'WordPress.org', 'social-count-plus' ) . '</a>' ); ?></p>
+	<p><?php echo esc_html( sprintf( __( 'Help us keep the %s plugin free making a donation or rate %s on WordPress.org. Thank you in advance!', 'social-count-plus' ), __( 'Social Count Plus', 'social-count-plus' ), '&#9733;&#9733;&#9733;&#9733;&#9733;' ) ); ?></p>
+		<p><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=Y8HP99ZJ5Z59L" target="_blank" class="button button-primary"><?php esc_html_e( 'Make a donation', 'social-count-plus' ); ?></a> <a href="https://wordpress.org/support/view/plugin-reviews/social-count-plus?filter=5#postform" target="_blank" class="button button-secondary"><?php esc_html_e( 'Make a review', 'social-count-plus' ); ?></a></p>
 </div>
